23fc55b2cd59e07d7f6581239174f2c293744405
Norweig1an <287521367@qq.com>
2025-04-05 17:28:06 +0000

    Deploying to gh-pages from @ Norweig1an/Norweig1an.github.io@b0efeb75ba5303342d1e3292f2b14cb63a866414 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -437,13 +437,6 @@ ninja.data = [{
           window.open("https://github.com/Norweig1an", "_blank");
         },
       },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/feed.xml", "_blank");
-        },
-      },{
         id: 'social-scholar',
         title: 'Google Scholar',
         section: 'Socials',
