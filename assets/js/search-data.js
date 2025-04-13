2225f413c34e30b5891510192d0d2ec493b641b4
Norweig1an <287521367@qq.com>
2025-04-13 07:37:41 +0000

    Deploying to gh-pages from @ Norweig1an/Norweig1an.github.io@2a33622103091b2ebb8f2cb18daa3feb5712edf5 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -3,8 +3,8 @@ const ninja = document.querySelector('ninja-keys');
 
 // add the home and posts menu items
 ninja.data = [{
-    id: "nav-about",
-    title: "about",
+    id: "nav-shengcheng-luo-罗盛成",
+    title: "Shengcheng Luo | 罗盛成",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
