3f7cc4c5df6cde6780b352028914ae1228299119
WoodRoof Luo <woodroof@WoodRoofdeMacBook-Air.local>
2026-09-07 02:06:38 +0800

    Preserve image zoom layers in production CSS

--- purgecss.config.js
+++ purgecss.config.js
@@ -2,5 +2,6 @@ module.exports = {
   content: ["_site/**/*.html", "_site/**/*.js"],
   css: ["_site/assets/css/*.css"],
   output: "_site/assets/css/",
+  safelist: ["medium-zoom-overlay", "medium-zoom-image--opened"],
   skippedContentGlobs: ["_site/assets/**/*.html"],
 };
